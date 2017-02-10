c330d515a1b86d389e1cbe03f8fa25cc80eca546
stoffern <kristoffer.klevenberg@gmail.com>
2017-02-10 09:18:31 +0100

    feat(DataLoader): Added override with DataLoader on .get($connection)

--- src/composeWithDataLoader.js
+++ src/composeWithDataLoader.js
@@ -1,11 +1,7 @@
 import { TypeComposer } from 'graphql-compose'
 import DataLoader from 'dataloader'
-import {
-  wrapFindById,
-  wrapFindByIds,
-  wrapConnection,
-  wrapFindMany,
-} from './resolvers'
+import hash from 'object-hash'
+import SingleContinous from './singleContinous'
 
 import {
   dataloaderOptions
@@ -20,14 +16,116 @@ export function composeWithDataLoader(
   // if (!(typeComposer instanceof TypeComposer)) {
   //   throw new Error('You should provide TypeComposer instance to composeWithDataLoader method');
   // }
+  // 
+  // 
+  /**
+   * Set defaults
+   */
+  options ={
+    cacheExpiration: options.cacheExpiration || 300,
+    removeProjection: options.removeProjection || true,
+    debug: options.debug || false,
+  } 
 
   /**
-   * get resolvers to add DataLoader to
+   * Add DataLoader to FindById
    */
-  typeComposer = wrapFindById(typeComposer, options)
-  typeComposer = wrapFindByIds(typeComposer, options)
-  typeComposer = wrapFindMany(typeComposer, options)
-  typeComposer = wrapConnection(typeComposer, options)
+  let findByIdResolver = typeComposer.getResolver('findById')
+  let findByIdLoader = new DataLoader( (resolveParamsArray) => {
+    if (options.debug) console.log('New db request (findById)')
+    let params = resolveParamsArray[0]
+
+    return findByIdResolver.resolve(params).then(res => [res])
+  },
+  { cacheKeyFn: key => key.args._id.toString() })
+
+  typeComposer.setResolver( 'findById', 
+    findByIdResolver.wrapResolve(fn => rp => {
+      if (options.removeProjection) delete rp.projection
+      SingleContinous.run(findByIdLoader, rp, options)
+      return findByIdLoader.load(rp)
+    })
+  )
+
+
+
+  /**
+   * Add DataLoader to FindByIds
+   */
+  // let findByIdsResolver = typeComposer.getResolver('findByIds')
+  // let findByIdsLoader = new DataLoader( (resolveParamsArray) => {
+  //   if (options.debug) console.log('New db request (findByIds)')
+  //   return findByIdResolver.resolve(resolveParamsArray[0]).then(res => [res])
+  // },
+  // { cacheKeyFn: key => {
+  //   let hashKey = hash(key.args)
+  //   return hashKey
+  // } })
+
+  // typeComposer.setResolver(
+  //   'findByIds', 
+  //   findByIdsResolver.wrapResolve(fn => rp => {
+  //     SingleContinous.run(findByIdsLoader, rp, opt)
+  //     return findByIdsLoader.load(rp)
+  //   })
+  // )
+  
+  
+
+  /**
+   * Add DataLoader to FindMany
+   */
+  // let findManyResolver = typeComposer.getResolver('findMany')
+  // let findManyLoader = new DataLoader( (resolveParamsArray) => {
+  //   if (options.debug) console.log('New db request (findMany)')
+  //   console.log(resolveParamsArray[0])
+  //   //response
+  //   return findManyResolver.resolve(resolveParamsArray[0]).then(res => [res])
+  // },
+  // { cacheKeyFn: key => {
+  //   let hashKey = hash(key.args)
+  //   return hashKey
+  // } })
+
+  // typeComposer.setResolver(
+  //   'findMany', 
+  //   findManyResolver.wrapResolve(fn => rp => {
+  //     if (options.removeProjection) delete rp.projection
+  //     SingleContinous.run(findManyLoader, rp, options)
+  //     return findManyLoader.load(rp)
+  //   })
+  // )
+
+
+  /**
+   * Add DataLoader to Connection
+   */
+  let connectionResolver = typeComposer.getResolver('connection')
+  let connectionFieldNames = typeComposer.getFieldNames()
+  let connectionLoader = new DataLoader( (resolveParamsArray) => {
+    if (options.debug) console.log('New db request (connection)')
+    let params = resolveParamsArray[0]
+    //response
+    return connectionResolver.resolve(params).then(res => [res])
+  },
+  { cacheKeyFn: key => {
+    let hashKey = hash(key.args)
+    return hashKey
+  } })
+
+  typeComposer.setResolver( 'connection', 
+    connectionResolver.wrapResolve(fn => rp => {
+      if(options.removeProjection){
+        let projection ={ edges: { node: {} } }
+        connectionFieldNames.map( field => projection.edges.node[field] = true)
+        rp.projection = projection
+      }
+      SingleContinous.run(connectionLoader, rp, options)
+      return connectionLoader.load(rp)
+    })
+  )
+
+
 
   return typeComposer
 }
--- src/definitions.js
+++ src/definitions.js
@@ -1,4 +1,5 @@
 export type dataloaderOptions = {
   cacheExpiration?: number,
+  removeProjection?: bool,
   debug?: bool,
 }
